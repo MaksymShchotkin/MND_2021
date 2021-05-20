3e39e91ed2e1aa35610f11a633401f37169ce450
Максим Щоткин ИВ-91 <42338712+MaksymShchotkin@users.noreply.github.com>
2021-05-20 22:00:38 +0300

    commit to lab 2
    @ehidniy_kisel it's his fault

--- Lab2/MND2.py
+++ Lab2/MND2.py
@@ -1,96 +1,169 @@
-import random as rand
-from numpy import linalg as lg
+from random import randint
+import numpy as np
 
-x1_min = 10     #-1
-x1_max = 50     #1
-x2_min = 20     #-1
-x2_max = 60     #1
 
-y_max = (30-130)*10
-y_min = (20-130)*10
-print("y_max: "+str(y_max),"\ny_min: "+str(y_min), '\n '  )
-a1 = [rand.randint(y_min, y_max) for i in range(5)]
-a2 = [rand.randint(y_min, y_max) for i in range(5)]
-a3 = [rand.randint(y_min, y_max) for i in range(5)]
-print("Y1: "+str(a1), "\nY2: "+str(a2),"\nY3: "+str(a3), '\n ' )
-y_aver1 = sum(a1) / len(a1)
-y_aver2 = sum(a2) / len(a2)
-y_aver3 = sum(a3) / len(a3)
-print("Середній Y 1: "+str(y_aver1),"\nСередній Y 2: "+str(y_aver2),"\nСередній Y 3: "+str(y_aver3),'\n ')
-a1_vidhul = [y_aver1 - a1[i] for i in range(len(a1))]
-a2_vidhul = [y_aver1 - a2[i] for i in range(len(a2))]
-a3_vidhul = [y_aver1 - a3[i] for i in range(len(a3))]
-print("a1: "+str(a1_vidhul),"\na2: "+str(a2_vidhul),"\na3: "+str(a3_vidhul),'\n ')
+def normalize(array):
+    x0 = (max(array) + min(array)) / 2
+    dx = x0 - min(array)
+    normalized = [(array[i] - x0) / dx for i in range(len(array))]
+    return normalized
 
-a1_vidh_kvad = []
-a2_vidh_kvad = []
-a3_vidh_kvad = []
-for i in range(len(a1)):
-    a1_vidh_kvad.append(a1_vidhul[i] ** 2)
-    a2_vidh_kvad.append(a2_vidhul[i] ** 2)
-    a3_vidh_kvad.append(a3_vidhul[i] ** 2)
-print("a1 квадратичне: "+str(a1_vidh_kvad), "\na2 Середній Y : "+str(a2_vidh_kvad),"\na3 Середній Y : "+str(a3_vidh_kvad), '\n')
-a1_disp = sum(a1_vidh_kvad) / len(a1_vidh_kvad)
-a2_disp = sum(a2_vidh_kvad) / len(a2_vidh_kvad)
-a3_disp = sum(a3_vidh_kvad) / len(a3_vidh_kvad)
-print("a1 дисперсія: "+str(a1_disp), "\na2 дисперсія: "+str(a2_disp),"\na3 дисперсія: "+str(a3_disp),'\n')
-a1_disp_perc = a1_disp / (a1_disp + a2_disp + a3_disp)
-a2_disp_perc = a2_disp / (a1_disp + a2_disp + a3_disp)
-a3_disp_perc = a3_disp / (a1_disp + a2_disp + a3_disp)
-print("a1 регрессія: "+str(a1_disp_perc),"a2 регрессія: "+str(a2_disp_perc), "a3 регрессія: "+str(a3_disp_perc), '\n ')
-Fuv1 = a1_disp / a2_disp
-Fuv2 = a3_disp / a1_disp
-Fuv3 = a3_disp / a2_disp
-print("Fuv1: "+str(Fuv1),"\nFuv2: "+str(Fuv2),"\nFuv3: "+str(Fuv3),'\n')
-Ouv1 = 3/5*Fuv1
-Ouv2 = 3/5*Fuv2
-Ouv3 = 3/5*Fuv3
-print("Ouv1: "+str(Ouv1),"\nOuv2: "+str(Ouv2),"\nOuv2\ 3: "+str(Ouv3),'\n')
-Ruv1 = abs(Ouv1 - 1)/1.79
-Ruv2 = abs(Ouv2 - 1)/1.79
-Ruv3 = abs(Ouv3 - 1)/1.79
-print("Ruv1: "+str(Ruv1),"\nRuv2: "+str(Ruv2),"\nRuv3: "+str(Ruv3))
-print(str(Ruv1)+"<Rkr = 2\n",str(Ruv2)+"<Rkr = 2\n",str(Ruv3)+"<Rkr = 2\n")
-mx1 = (-1+1+(-1))/3
-mx2 = (-1+(-1)+1)/3
-my = (y_aver1 + y_aver2+ y_aver3)/3
-print("mx1: "+str(mx1),"\nmx2: "+str(mx2),"\nmy: "+str(my),'\n')
-A1 = (1+1+1)/3
-A2 = (1-1-1)/3
-A3 = (1+1+1)/3
-print("A1: "+str(A1),"\nA2: "+str(A2),"\nA3: "+str(A3),'\n')
-A11 =(-1*y_aver1+1*y_aver2-1*y_aver3)/3
-A22 =(-1*y_aver1-1*y_aver2+1*y_aver3)/3
-print("A11: "+str(A11),"\nA22: "+str(A22),'\n')
-b0 = (lg.det([[my, mx1, mx2],
-                 [A11, A1, A2],
-                 [A22, A2, A3]]))/(lg.det([[1, mx1, mx2],
-                                               [mx1, A1, A2],
-                                               [mx2, A2, A3]]))
-b1 = (lg.det([[1, my, mx2],
-                 [mx1, A11, A2],
-                 [mx2, A22, A3]]))/(lg.det([[1, mx1, mx2],
-                                               [mx1, A1, A2],
-                                               [mx2, A2, A3]]))
-b2 = (lg.det([[1, mx1, my],
-                 [mx1, A1, A11],
-                 [mx2, A2, A22]]))/(lg.det([[1, mx1, mx2],
-                                               [mx1, A1, A2],
-                                               [mx2, A2, A3]]))
-print("b0: "+str(b0),"\nb1: "+str(b1),"\nb2: "+str(b2),'\n')
-print("Y1 = : "+str(b0 + b1*-1 + b2*(-1)),"\nY2 = : "+str(b0 + b1*1 + b2*(-1)),"\nY3 = : "+str(b0 + b1*(-1) + b2*1))
-print("Y = "+str(b0)+" + "+str(b1)+"*x1 + "+str(b2)+"*x2")
-Dx1 = abs(x1_max-x1_min)/2
-Dx2 = abs(x2_max-x2_min)/2
-x10 = (x1_max+x1_min)/2
-x20 = (x2_max+x2_min)/2
-print("Dx1: "+str(Dx1),"\nDx2: "+str(Dx2),"\nx10: "+str(x10),'\nx20:'+str(x20),'\n')
-a0 = b0-b1*(x10/Dx1)-b2*(x20/Dx2)
-a1 = b1/Dx1
-a2 = b2/Dx2
-print("a0: "+str(a0),"\na1: "+str(a1),"\na2: "+str(a2),'\n')
-print("Ynat1 = : "+str(a0 + a1*x1_min + a2*x2_min))
-print("Ynat2 = : "+str(a0 + a1*x1_max + a2*x2_min))
-print("Ynat3 = : "+str(a0 + a1*x1_min + a2*x2_max))
-print("Ynat = "+str(a0)+" + "+str(a1)+"*x1 + "+str(a2)+"*x2")
-print(" ")
+
+# Підбір найближчого значення для m
+def find_closest(m, table):
+    key_list = list(table.keys())
+    if m in key_list:
+        return m
+    else:
+        diffs = ([abs(m-i) for i in key_list])
+        index = diffs.index(min(diffs))
+        return key_list[index]
+
+
+def main(m):
+    var = 130
+    y_max = (30 - var) * 10
+    y_min = (20 - var) * 10
+    print(f'y_max = {y_max}, y_min = {y_min}')
+
+    # Таблиця з критеріями Романовського для p = 0.99
+    romanovski_values_table = {2: 1.73, 6: 2.16, 8: 2.43, 10: 2.62, 12: 2.75, 15: 2.9, 20: 3.08}
+
+    x1 = [10, randint(10, 50), 50]
+    x2 = [20, randint(20, 60), 60]
+    print(f'X1: {x1}')
+    print(f'X2: {x2}')
+
+    x1_n = normalize(x1)
+    x2_n = normalize(x2)
+    print(f'X1 normalized: {x1_n}')
+    print(f'X2 normalized: {x2_n}')
+
+    matrix_of_experiments = [[randint(y_min, y_max) for _ in range(3)] for _ in range(m)]
+    print(f'Y from 1 to m: {matrix_of_experiments}')
+
+    y_average = []
+    dispersions = []
+
+    for i in range(len(matrix_of_experiments[0])):
+        sum1 = 0
+        for j in range(m):
+            sum1 += matrix_of_experiments[j][i]
+
+        y_current = sum1 / len(matrix_of_experiments)
+        y_average.append(y_current)
+
+        sum2 = 0
+        for k in range(m):
+            sum2 += (matrix_of_experiments[k][i] - y_current) ** 2
+        dispersions.append(sum2 / m)
+
+    print(f'Y average: {y_average}')
+    print(f'Dispersions: {dispersions}')
+
+    main_dispersion = ((2 * (2 * m - 2)) / (m * (m - 4))) ** (1 / 2)
+    print(f'Main dispersion = {main_dispersion}')
+
+    f_uv = []
+    theta_uv = []
+    r_uv = []
+    res = 0
+    for i in range(3):
+        for j in range(i + 1, 3):
+            if dispersions[i] >= dispersions[j]:
+                res = dispersions[i] / dispersions[j]
+            else:
+                res = dispersions[j] / dispersions[i]
+            f_uv.append(res)
+
+        theta = ((m - 2) / m) * res
+        theta_uv.append(theta)
+
+        r_uv.append(abs(theta - 1) / main_dispersion)
+
+    print(f'Fuv : {f_uv}')
+    print(f'0uv: {theta_uv}')
+    print(f'Ruv: {r_uv}')
+
+    r_kr = romanovski_values_table[find_closest(m, romanovski_values_table)]
+    print(f'При m = {m} для p = 0.99 Rkr = {r_kr}')
+    for i in r_uv:
+        if i >= r_kr:
+            print('Дисперсії неоднорідні')
+            print(f'Проводимо експеримент при m = {m+1}')
+            main(m+1)
+            exit()
+    print('Дисперсії однорідні')
+
+    mx1 = sum(x1_n) / 3
+    mx2 = sum(x2_n) / 3
+    my = sum(y_average) / 3
+
+    print(f'mx1: {mx1}')
+    print(f'mx2: {mx2}')
+    print(f'my: {my}')
+
+    a1 = (x1_n[0] ** 2 + x1_n[1] ** 2 + x1_n[2] ** 2) / 3
+    a2 = (x1_n[0] * x2_n[0] + x1_n[1] * x2_n[1] + x1_n[2] * x2_n[2]) / 3
+    a3 = (x2_n[0] ** 2 + x2_n[1] ** 2 + x2_n[2] ** 2) / 3
+    print(f'a1 = {a1}')
+    print(f'a2 = {a2}')
+    print(f'a3 = {a3}')
+
+    a11 = (x1_n[0] * y_average[0] + x1_n[1] * y_average[1] + x1_n[2] * y_average[2]) / 3
+    a22 = (x2_n[0] * y_average[0] + x2_n[1] * y_average[1] + x2_n[2] * y_average[2]) / 3
+    print(f'a11 = {a11}')
+    print(f'a22 = {a22}')
+
+    b0 = np.linalg.det([[my, mx1, mx2],
+                        [a11, a1, a2],
+                        [a22, a2, a3]]) / \
+         np.linalg.det([[1, mx1, mx2],
+                        [mx1, a1, a2],
+                        [mx2, a2, a3]])
+
+    b1 = np.linalg.det([[1, my, mx2],
+                        [mx1, a11, a2],
+                        [mx2, a22, a3]]) / \
+         np.linalg.det([[1, mx1, mx2],
+                        [mx1, a1, a2],
+                        [mx2, a2, a3]])
+
+    b2 = np.linalg.det([[1, mx1, my],
+                        [mx1, a1, a11],
+                        [mx2, a2, a22]]) / \
+         np.linalg.det([[1, mx1, mx2],
+                        [mx1, a1, a2],
+                        [mx2, a2, a3]])
+
+    print(f'Нормоване рівняння регресії: \ny = {b0} + {b1}*x1 + {b2}*x2')
+    for i in range(3):
+        print(f'Для X1{i + 1} та X2{i + 1}')
+        print(f'y = {b0 + b1 * x1_n[i] + b2 * x2_n[i]}')
+
+    dx1 = abs(x1[2] - x1[0]) / 2
+    dx2 = abs(x2[2] - x2[0]) / 2
+    x10 = (x1[2] + x1[0]) / 2
+    x20 = (x2[2] + x2[0]) / 2
+    print(f'dx1 = {dx1}')
+    print(f'dx2 = {dx2}')
+    print(f'x10 = {x10}')
+    print(f'x20 = {x20}')
+
+    a0_res = b0 - b1 * (x10 / dx1) - b2 * (x20 / dx2)
+    a1_res = b1 / dx1
+    a2_res = b2 / dx2
+    print(f'a0 = {a0_res}')
+    print(f'a1 = {a1_res}')
+    print(f'a2 = {a2_res}')
+
+    print(f'Натуралізоване рівняння регресії: \ny = {a0_res} + {a1_res}*x1 + {a2_res}*x2')
+    for i in range(3):
+        print(f'Для X1{i + 1} та X2{i + 1}')
+        print(f'y = {a0_res + a1_res * x1[i] + a2_res * x2[i]}')
+
+
+if __name__ == '__main__':
+    main(m=5)
+
+
